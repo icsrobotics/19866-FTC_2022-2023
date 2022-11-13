35d67380bd769655bac3fa6a8483b5f08d3e30c0
programming-programmer <mthich15@gmail.com>
2022-11-12 20:29:59 -0800

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandbox.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandbox.java
@@ -21,11 +21,10 @@ public class Sandbox extends LinearOpMode{
     public void runOpMode() throws InterruptedException {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = dashboard.getTelemetry();
-        int level = 0;
 
         telemetry.setMsTransmissionInterval(50);
 
-//        robot.init();
+        robot.init();
         waitForStart();
 
         while (opModeIsActive()) {
@@ -38,13 +37,22 @@ public class Sandbox extends LinearOpMode{
             double frPower = pidControl.PIDControl(targetPosition, robot.frontRight.getCurrentPosition());
 
 */
-            if (gamepad2.dpad_up) {
-                telemetry.addData("Dpad Up", "Pressed");
-                level = level + 1;
-                telemetry.addData("Level", level);
+            // Low: 1936, Med: 2770, High: 4075
+            int lowTargetPosition = 1936;
+            int midTargetPosition = 2770;
+            int highTargetPosition = 4075;
 
-                telemetry.update();
-            }
+            double leftArmPower = pidControl.PIDControl(lowTargetPosition, robot.leftArmMotor.getCurrentPosition());
+            double rightArmPower = pidControl.PIDControl(lowTargetPosition, robot.rightArmMotor.getCurrentPosition());
+
+            robot.leftArmMotor.setPower(leftArmPower);
+            robot.rightArmMotor.setPower(rightArmPower);
+
+            telemetry.addData("Left Arm Motor Position", robot.leftArmMotor.getCurrentPosition());
+            telemetry.addData("Right Arm Motor Position", robot.rightArmMotor.getCurrentPosition());
+
+            telemetry.update();
         }
     }
 }
+
