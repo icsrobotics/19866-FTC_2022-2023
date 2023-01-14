676c966cb4681071e49474d3a3338e48dfd3bbe3
programming-programmer <mthich15@gmail.com>
2023-01-14 08:31:23 -0800

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandbox.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sandbox.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @Config
-@TeleOp(name = "Sandbox", group = "Linear Opmode")
+@Autonomous(name = "Move Forward", group = "Linear Opmode")
 public class Sandbox extends LinearOpMode {
     int oneTile = 1075;
 
@@ -27,10 +28,10 @@ public class Sandbox extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            frPower = DrivePIDController.PIDControl(oneTile, robot.frontRight.getCurrentPosition(), 10);
-            flPower = DrivePIDController.PIDControl(oneTile, robot.frontLeft.getCurrentPosition(), 10);
-            brPower = DrivePIDController.PIDControl(oneTile, robot.backRight.getCurrentPosition(), 10);
-            blPower = DrivePIDController.PIDControl(oneTile, robot.backLeft.getCurrentPosition(), 10);
+            frPower = DrivePIDController.PIDControl(oneTile, robot.frontRight.getCurrentPosition(), 5);
+            flPower = DrivePIDController.PIDControl(oneTile, robot.frontLeft.getCurrentPosition(), 5);
+            brPower = DrivePIDController.PIDControl(oneTile, robot.backRight.getCurrentPosition(), 5);
+            blPower = DrivePIDController.PIDControl(oneTile, robot.backLeft.getCurrentPosition(), 5);
 
             robot.frontRight.setPower(-frPower);
             robot.frontLeft.setPower(-flPower);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DefaultAutoWithoutHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DefaultAutoWithoutHardware.java
@@ -3,33 +3,35 @@ package org.firstinspires.ftc.teamcode.autonomous;
 // Refrenced code: https://github.com/cobalt-colts/AprilTag-Workshop/blob/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AprilTagAutonomousInitDetectionExample.java
 
 
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RESET_ENCODERS;
-
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.MyPIDController;
+import org.firstinspires.ftc.teamcode.RobotHardware;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.firstinspires.ftc.teamcode.RobotHardware;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.robot.Robot;
-
-import org.firstinspires.ftc.teamcode.MyPIDController;
 
 import java.util.ArrayList;
 
 @Config
-@Autonomous(name = "Defualt Autonomous", group = "Linear Opmode")
-public class DefaultAuto extends LinearOpMode {
+@Autonomous(name = "Other... Autonomous", group = "Linear Opmode")
+public class DefaultAutoWithoutHardware extends LinearOpMode {
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
     RobotHardware robot = new RobotHardware(this);
     MyPIDController DrivePIDController = new MyPIDController(0.07, 0.05, 0.01);
+
+    // Drive Motors. Ports
+    public DcMotorEx frontLeft = null;
+    public DcMotorEx frontRight = null;
+    public DcMotorEx backRight = null;
+    public DcMotorEx backLeft = null;
 
     static final double FEET_PER_METER = 3.28084;
 
@@ -51,6 +53,26 @@ public class DefaultAuto extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        backLeft = (DcMotorEx) hardwareMap.dcMotor.get("Back_Left");
+        backRight = (DcMotorEx) hardwareMap.dcMotor.get("Back_Right");
+        frontRight = (DcMotorEx) hardwareMap.dcMotor.get("Front_Right");
+        frontLeft = (DcMotorEx) hardwareMap.dcMotor.get("Front_Left");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
+        frontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
@@ -137,7 +159,7 @@ public class DefaultAuto extends LinearOpMode {
             telemetry.update();
         }
 
-        robot.init();
+        init();
         /* Actually do something useful */
 
         // GAMEPLAN!!
@@ -154,36 +176,36 @@ public class DefaultAuto extends LinearOpMode {
         //Drivetrain values
         double oneTile = 1000;
 
-        double frPower = DrivePIDController.PIDControl(oneTile, robot.frontRight.getCurrentPosition(), 10);
-        double flPower = DrivePIDController.PIDControl(oneTile, robot.frontLeft.getCurrentPosition(), 10);
-        double brPower = DrivePIDController.PIDControl(oneTile, robot.backRight.getCurrentPosition(), 10);
-        double blPower = DrivePIDController.PIDControl(oneTile, robot.backLeft.getCurrentPosition(), 10);
+        double frPower = DrivePIDController.PIDControl(oneTile, frontRight.getCurrentPosition(), 10);
+        double flPower = DrivePIDController.PIDControl(oneTile, frontLeft.getCurrentPosition(), 10);
+        double brPower = DrivePIDController.PIDControl(oneTile, backRight.getCurrentPosition(), 10);
+        double blPower = DrivePIDController.PIDControl(oneTile, backLeft.getCurrentPosition(), 10);
 
         if(tagOfInterest == null) {
             //default trajectory here if preferred
             // FORWARD
-            robot.frontRight.setPower(frPower);
-            robot.frontLeft.setPower(flPower);
-            robot.backRight.setPower(brPower);
-            robot.backLeft.setPower(blPower);
+            frontRight.setPower(frPower);
+            frontLeft.setPower(flPower);
+            backRight.setPower(brPower);
+            backLeft.setPower(blPower);
 
         } else if(tagOfInterest.id == LEFT) {
             //left trajectory - 1
 
             // FORWARD
-            robot.frontRight.setPower(frPower);
-            robot.frontLeft.setPower(flPower);
-            robot.backRight.setPower(brPower);
+            frontRight.setPower(frPower);
+            frontLeft.setPower(flPower);
+            backRight.setPower(brPower);
 
-            robot.backLeft.setPower(blPower);
+            backLeft.setPower(blPower);
 
             sleep(500);
 
             //LEFT
-            robot.frontRight.setPower(frPower);
-            robot.frontLeft.setPower(-flPower);
-            robot.backRight.setPower(-brPower);
-            robot.backLeft.setPower(blPower);
+            frontRight.setPower(frPower);
+            frontLeft.setPower(-flPower);
+            backRight.setPower(-brPower);
+            backLeft.setPower(blPower);
 
             telemetry.addData("Robot", "LEFT");
             telemetry.update();
@@ -191,10 +213,10 @@ public class DefaultAuto extends LinearOpMode {
             //middle trajectory - 2
 
             // FORWARD
-            robot.frontRight.setPower(frPower);
-            robot.frontLeft.setPower(flPower);
-            robot.backRight.setPower(brPower);
-            robot.backLeft.setPower(blPower);
+            frontRight.setPower(frPower);
+            frontLeft.setPower(flPower);
+            backRight.setPower(brPower);
+            backLeft.setPower(blPower);
 
             telemetry.addData("Robot", "MIDDLE");
             telemetry.update();
@@ -202,18 +224,18 @@ public class DefaultAuto extends LinearOpMode {
             //right trajectory - 3
 
             // FORWARD
-            robot.frontRight.setPower(frPower);
-            robot.frontLeft.setPower(flPower);
-            robot.backRight.setPower(brPower);
-            robot.backLeft.setPower(blPower);
+            frontRight.setPower(frPower);
+            frontLeft.setPower(flPower);
+            backRight.setPower(brPower);
+            backLeft.setPower(blPower);
 
             sleep(500);
 
             //RIGHT
-            robot.frontRight.setPower(-frPower);
-            robot.frontLeft.setPower(flPower);
-            robot.backRight.setPower(brPower);
-            robot.backLeft.setPower(-blPower);
+            frontRight.setPower(-frPower);
+            frontLeft.setPower(flPower);
+            backRight.setPower(brPower);
+            backLeft.setPower(-blPower);
 
             telemetry.addData("Robot", "RIGHT");
             telemetry.update();
